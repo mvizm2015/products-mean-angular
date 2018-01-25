191e3e95c4fc3068fed49d89c4742561f39a751e
mvizm2015 <mariaviz2015@gmail.com>
2018-01-25 15:00:43 -0500

    App Component initial html and ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+// Import the DataService
+import { DataService } from './data.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Products Store';
+
+  // Define a products property to hold our product data
+  products: Array<any>;
+
+  // Create an instance of the DataService through dependency injection
+    constructor(private _dataService: DataService) {
+
+    // Access the Data Service's getProducts() method we defined
+    this._dataService.getProducts()
+      .subscribe(res => this.products = res);
+  }
 }
