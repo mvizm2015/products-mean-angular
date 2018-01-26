853fad62cfdecfdc1a2bc6e3bc4700027b390fc4
mvizm2015 <mariaviz2015@gmail.com>
2018-01-25 20:52:35 -0500

    Add service product

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Product } from './../product';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  @Input() product: Product;
 
   constructor() { }
 
