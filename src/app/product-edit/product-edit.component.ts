819e02fb5175df80df8723a622e70ca676d9b429
mvizm2015 <mariaviz2015@gmail.com>
2018-01-31 23:55:20 -0500

    Edit component

--- src/app/product-edit/product-edit.component.ts
+++ src/app/product-edit/product-edit.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { DataService } from './../data.service';
+import { Product } from './../product';
+import { Component, OnInit, Input, Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { PRODUCTS } from './../mock-products';
 
 @Component({
   selector: 'app-product-edit',
@@ -6,10 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-edit.component.css']
 })
 export class ProductEditComponent implements OnInit {
+  products = PRODUCTS;
 
-  constructor() { }
+  selectedProduct: Product;
+
+  constructor(
+    private route: ActivatedRoute,
+    private dataService: DataService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.getProduct(this.route.snapshot.params['id']);
+  }
+
+  getProduct(id) {
+    this.dataService.get('/product/'+id).subscribe(data => {
+      this.product = data;
+    })
+  }
+
+  updateProduct(id, data) {
+    this.dataService.put('/product/'+id, data)
+      .subscribe(res => {
+          let id = res['_id'];
+          this.route.navigate(['/product-details', id]);
+        }, (err) => {
+          console.log(err);
+        }
+      );
   }
 
 }
